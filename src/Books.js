babd9fcbf7e3d74ec0d11f15856676c814b15d3a
James Woszczynski <jameswoszczynski@gmail.com>
2018-08-19 10:04:38 +0100

    Split app into Components

---
+++ src/Books.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+class Books extends Component {}
+
+export default Books;
---
+++ src/MainPage.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+class MainPage extends Component {}
+
+export default MainPage;
